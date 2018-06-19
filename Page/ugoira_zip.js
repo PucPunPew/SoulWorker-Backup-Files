19b5eb4e1a2a4473f234cefb75d7c87c29d3d5c6
Dramiel Leayal <PucPunPew@users.noreply.github.com>
2018-06-19 09:51:12 +0700

    Update ugoira_zip.js

--- Page/ugoira_zip.js
+++ Page/ugoira_zip.js
@@ -1,80 +1,120 @@
-(function() {
-    ["jszip.min.js"].forEach(function(value) {
+(function () {
+    ["jszip.min.js"].forEach(function (value) {
         var script = document.createElement("script");
         script.src = "https://pucpunpew.github.io/SoulWorker-Backup-Files/Page/" + value;
         document.body.appendChild(script);
     });
-    var title = document.querySelector(".work-info .title");
-    title.appendChild(document.createElement("br"));
-    [{
-        name: "Zip",
-        data: pixiv.context.ugokuIllustData
-    },{
-        name: "ZipHQ",
-        data: pixiv.context.ugokuIllustFullscreenData
-    }].forEach(function(value) {
-        var elem, elemtxt, click;
-        click = function() {
-            if ("removeEventListener" in elem) {
-                elem.removeEventListener("click", click);
-            } else {
-                elem.detachEvent("onclick", click);
-            }
-            var basename = pixiv.context.illustId + "_" + pixiv.context.illustTitle.replace(/[\\/:*?"<>|\u00b7]/g, "");
-            var savename = basename + value.name + ".ugoira";
-            if (typeof Blob === "undefined"){
-                elemtxt.nodeValue = " >Save" + value.name + "< ";
-                elem.href = value.data.src;
-                elem.download = savename;
-            } else {
-                elemtxt.nodeValue = " >Loading" + value.name + "...< ";
-                var xhr = new XMLHttpRequest();
-                xhr.open("GET", value.data.src);
-                xhr.responseType = "arraybuffer";
-                xhr.addEventListener("load", function() {
-                    elemtxt.nodeValue = " >Save" + value.name + "< ";
-					JSZip.loadAsync(xhr.response).then(function(zip) {
-						zip.file("animation.json", JSON.stringify(pixiv.context));
-						
-						zip.generateAsync({type:"blob"}).then(function(blob) {
-							if ("msSaveOrOpenBlob" in window.navigator) {
-								elem.addEventListener("click", function() {
-									window.navigator.msSaveOrOpenBlob(blob, savename);
-								});
-							} else {
-								elem.href = window.URL.createObjectURL(blob);
-								elem.download = savename;
-							}
-						});
-					});
-                    /* 
-					var zip = new JSZip(xhr.response);
-					var blob = zip.generate({type:"blob"});
-                    if ("msSaveOrOpenBlob" in window.navigator) {
-                        elem.addEventListener("click", function() {
-                            window.navigator.msSaveOrOpenBlob(blob, savename);
-                        });
-                    } else {
-                        elem.href = window.URL.createObjectURL(blob);
-                        elem.download = savename;
-                    }
-					*/
-                });
-                xhr.addEventListener("error", function() {
-                    elemtxt.nodeValue = " >Error" + value.name + "< ";
-                });
-                xhr.send();
-            }
-        };
-        elem = document.createElement("a");
-        elemtxt = document.createTextNode(" >DL" + value.name + "< ");
-        elem.appendChild(elemtxt);
-        elem.className = "_button";
-        if ("addEventListener" in elem) {
-            elem.addEventListener("click", click);
-        } else {
-            elem.attachEvent("onclick", click);
+    var articleDom = document.querySelector("div div article"),
+        theline = document.createElement("br"),
+        ugoiraInfoTmp = window.globalInitData.preload.illust,
+        ugoiraInfo, key;
+    for (key in ugoiraInfoTmp)
+        if (ugoiraInfoTmp.hasOwnProperty(key) && ugoiraInfoTmp[key].illustId)
+            ugoiraInfo = ugoiraInfoTmp[key];
+
+    var authorInfoTmp = window.globalInitData.preload.user,
+        authorInfo;
+    for (key in authorInfoTmp)
+        if (authorInfoTmp.hasOwnProperty(key) && authorInfoTmp[key].name && authorInfoTmp[key].userId)
+            authorInfo = authorInfoTmp[key];
+    // "https://www.pixiv.net/ajax/illust/" + ugoiraInfo.illustId + "/ugoira_meta"
+
+    var xhrMain = new XMLHttpRequest();
+    xhrMain.open("GET", "https://www.pixiv.net/ajax/illust/" + ugoiraInfo.illustId + "/ugoira_meta");
+    xhrMain.responseType = "json";
+    xhrMain.addEventListener("load", function () {
+        var ugoiraMeta = xhrMain.response,
+            dummyJSONAnimation = {};
+        if (ugoiraMeta.mime) {
+
         }
-        title.appendChild(elem);
+        dummyJSONAnimation.illustComment = ugoiraInfo.illustComment;
+        dummyJSONAnimation.illustId = ugoiraInfo.illustId;
+        dummyJSONAnimation.illustTitle = ugoiraInfo.illustTitle;
+        dummyJSONAnimation.userId = authorInfo.userId;
+        dummyJSONAnimation.userName = authorInfo.name;
+        dummyJSONAnimation.ugokuIllustData = ugoiraMeta.body;
+        if (ugoiraMeta.error === false) {
+            articleDom.insertBefore(theline);
+            [{
+                name: "Zip",
+                data: ugoiraMeta.body.src
+            }, {
+                name: "ZipHQ",
+                data: ugoiraMeta.body.originalSrc
+            }].forEach(function (value) {
+                var elem, elemtxt, click;
+                click = function () {
+                    if ("removeEventListener" in elem) {
+                        elem.removeEventListener("click", click);
+                    } else {
+                        elem.detachEvent("onclick", click);
+                    }
+                    var basename = ugoiraInfo.illustId + "_" + ugoiraInfo.illustTitle.replace(/[\\/:*?"<>|\u00b7]/g, "");
+                    var savename = basename + value.name + ".ugoira";
+                    if (typeof Blob === "undefined") {
+                        elemtxt.nodeValue = " >Save" + value.name + "< ";
+                        elem.href = value.data;
+                        elem.download = savename;
+                    } else {
+                        elemtxt.nodeValue = " >Loading" + value.name + "...< ";
+                        var xhr = new XMLHttpRequest();
+                        xhr.open("GET", value.data);
+                        xhr.responseType = "arraybuffer";
+                        xhr.addEventListener("load", function () {
+                            elemtxt.nodeValue = " >Save" + value.name + "< ";
+                            JSZip.loadAsync(xhr.response).then(function (zip) {
+                                zip.file("animation.json", JSON.stringify(dummyJSONAnimation));
+
+                                zip.generateAsync({
+                                    type: "blob",
+                                    compression: "DEFLATE",
+                                    comment: dummyJSONAnimation.illustComment,
+                                    compressionOptions: {
+                                        level: 9
+                                    }
+                                }).then(function (blob) {
+                                    if ("msSaveOrOpenBlob" in window.navigator) {
+                                        elem.addEventListener("click", function () {
+                                            window.navigator.msSaveOrOpenBlob(blob, savename);
+                                        });
+                                    } else {
+                                        elem.href = window.URL.createObjectURL(blob);
+                                        elem.download = savename;
+                                    }
+                                });
+                            });
+                        });
+                        xhr.addEventListener("error", function () {
+                            elemtxt.nodeValue = " >Error" + value.name + "< ";
+                        });
+                        xhr.send();
+                    }
+                };
+                elem = document.createElement("a");
+                elemtxt = document.createTextNode(" >DL" + value.name + "< ");
+                elem.appendChild(elemtxt);
+                elem.className = "_button";
+                if ("addEventListener" in elem) {
+                    elem.addEventListener("click", click);
+                } else {
+                    elem.attachEvent("onclick", click);
+                }
+                theline.insertBefore(elem);
+            });
+        } else {
+            var elemtxt;
+            if (ugoiraMeta.message)
+                elemtxt = document.createTextNode("Error:" + ugoiraMeta.message);
+            else
+                elemtxt = document.createTextNode("Error:" + ugoiraMeta.error);
+            theline.appendChild(elemtxt);
+        }
     });
+    xhrMain.addEventListener("error", function () {
+        var elemtxt = document.createTextNode("Error: Cannot send request for ugoira metadata");
+        theline.appendChild(elemtxt);
+        // elemtxt.nodeValue = " >Error" + value.name + "< ";
+    });
+    xhrMain.send();
 })();
